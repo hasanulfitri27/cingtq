e328c3f6e9980ba9af49b64ecbeb94e5ac44b2f1
prok <78520555+hasanulfitri27@users.noreply.github.com>
2021-04-06 22:22:11 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Resume Hasanul Fitri</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Personal - v2.5.1
+  * Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header-tops">
+    <div class="container">
+
+      <h1><a href="index.html">CV CingYt</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a> -->
+      <h2>I'm a Human Professional, <span>I Believe To Allah,</span> I am from Lampeneurut</h2>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="#header">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#resume">Resume</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+
+      <div class="social-links">
+        
+        <a href="https://m.facebook.com/hasanulf1"><i class="icofont-facebook"></i></a>
+        <a href="https://www.instagram.com/hasanulfitriasyek/"><i class="icofont-instagram"></i></a>
+        
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= About Section ======= -->
+  <section id="about" class="about">
+
+    <!-- ======= About Me ======= -->
+    <div class="about-me container">
+
+      <div class="section-title">
+        <h2>Tentang</h2>
+        <p>Hidup Saya</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4" data-aos="fade-right">
+          <img src="assets/img/me.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+          <h3>Gamer</h3>
+          <p class="font-italic"></p>
+
+          <div class="row">
+            <div class="col-lg-8">
+              <ul>
+                <li><i class="icofont-rounded-right"></i> <strong>Nama Lengkap :</strong> Hasanul Fitri</li></strong>
+                <li><i class="icofont-rounded-right"></i> <strong>Alias        :</strong> CingYt</li>
+                <li><i class="icofont-rounded-right"></i> <strong>Tanggal Lahir:</strong> 29 November 2000</li>
+                <li><i class="icofont-rounded-right"></i> <strong>Alamat       :</strong> Indonesia, Aceh, Lampeneurut UB</li> 
+                <li><i class="icofont-rounded-right"></i> <strong>Agama        :</strong> Islam</li> 
+                <li><i class="icofont-rounded-right"></i> <strong>Pekerjaan    :</strong> Mahasiswa</li>
+                <li><i class="icofont-rounded-right"></i> <strong>Status       :</strong> Masih Single</li>
+              </ul>
+            </div>
+          
+          <p>
+            Ini adalah biografi hidup saya, Nama Lengkap saya Hasanul Fitri, banyak teman sekitar saya memanggil saya dengan sebutan CingYt, 
+            saya berasal dari Kota Aceh Besar tepatnya dari daerah Lampeneurut Ujong Blang, Saya anak kedua dari dua bersodara, Saya lahir pada
+            tanggal 29 November 2000 di Aceh Besar, Pekerjaan sekarang ialah saya masih Mahasiswa aktif di salah universitas swasta ternama yaitu 
+            Politeknik Aceh.
+
+          </p>
+        </div>
+      </div>
+
+    </div><!-- End About Me -->
+
+    <!-- ======= Testimonials ======= -->
+    <div class="testimonials container">
+
+      <div class="section-title">
+        <h2>Ini Kawan Bot Saya </h2>
+      </div>
+
+      <h3>Jak Maen Air Terjun Sigli</h3>
+      <img src="assets/img/testimonials/a.JPG" class="img-fluid" alt="">
+
+      <h3>Aksi Demo Simpang Lima</h3>
+      <img src="assets/img/testimonials/b.JPG" class="img-fluid" alt="">
+
+      <h3>Aksi Demo dan Foto Bersama </h3>
+      <img src="assets/img/testimonials/c.JPG" class="img-fluid" alt="">
+
+      <h3>Makan Lu gus</h3>
+      <img src="assets/img/testimonials/d.JPG" class="img-fluid" alt="">
+        
+      <h3>IT ni Boss </h3>
+      <img src="assets/img/testimonials/e.JPG" class="img-fluid" alt="">
+
+      <h3>Futsal Lu gus</h3>
+      <img src="assets/img/testimonials/f.JPG" class="img-fluid" alt="">
+
+      <h3>Makannn ni </h3>
+      <img src="assets/img/testimonials/g.JPG" class="img-fluid" alt="">
+      
+      <h3>Futsal Lu gus juara kagak</h3>
+      <img src="assets/img/testimonials/h.JPG" class="img-fluid" alt="">
+        
+
+
+    </div><!-- End Testimonials  -->
+
+  </section><!-- End About Section -->
+
+  <!-- ======= Resume Section ======= -->
+  <section id="resume" class="resume">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Resume</h2>
+        <p>Curiculum vitae</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6">
+          <h3 class="resume-title">Pendidikan Formal</h3>
+          <div class="resume-item pb-0">
+            <h4>TK</h4>
+            <h5>TK Bungong Padee 2005-2006</h5></div>
+          <div class="resume-item pb-0">
+            <h4>SD</h4>
+            <h5>SDN 1 Lampeneurut Unggul 2006-2012</h5></div>
+          <div class="resume-item pb-0">
+              <h4>MTsN</h4>
+              <h5>MtsN 2 Banda Aceh 2012-2015</h5></div>
+          <div class="resume-item pb-0">
+              <h4>SMK</h4>
+              <h5>SMKN 1 Banda Aceh 2015-2018</h5></div>
+
+
+          <h3 class="resume-title">Pendidikan Tambahan / Kursus</h3>
+            <div class="resume-item pb-0">
+              <h4>Program Praktek Industri (PPI) di Dinas Kependudukan dan Pencatatan Sipil</h4>
+              <h5>Banda Aceh Pada Tahun 2020</h5></div>  
+
+            <div class="resume-item pb-0">
+                <h4>Praktek Kerja Lapangan (PKL) di Kantor Kejaksanaan Tinggi Aceh</h4>
+                <h5>Banda Aceh Pada Tahun 2017</h5></div>
+
+            <div class="resume-item pb-0">
+                  <h4>Pelatihan Program Microsoft Office 2007 dan Program Peningkatan Kompetensi Keahlian</h4>
+                  <h5>Banda Aceh Pada Tahun 2017</h5></div>
+          
+            <div class="resume-item pb-0">
+                    <h4>Kursus Bahasa Germany</h4>
+                    <h5>Banda Aceh Pada Tahun 2017</h5></div>
+
+            <div class="resume-item pb-0">
+                      <h4>Pelatihan Pemasyarakatan Kewirausahaan Oleh Kementrian Koperasi dan UKM</h4>
+                      <h5>Banda Aceh Pada Tahun 2017</h5></div>
+          
+            <div class="resume-item pb-0">
+                        <h4>Pelatihan Tes Inteligensi Siswa Pada Program Pengembangan Mutu dan SDM</h4>
+                        <h5>Banda Aceh Pada Tahun 2013</h5></div>
+
+                        
+                          <h3 class="resume-title">Pengalaman Kerja</h3>
+                          <div class="resume-item pb-0">
+                            <h4>Kejaksanaan Tinggi Aceh</h4>
+                            <h5>Posisi Administrasi, Pada Tanggal 19 Juni 2017 - 19 September 2017</h5></div>
+                          <div class="resume-item pb-0">
+                            <h4>Dinas Kependudukan dan Pencatatan Sipil Kota Banda Aceh</h4>
+                            <h5>Posisi Operator, Pada Tanggal 27 Agustus 2020 - 27 Januari 2021</h5></div>
+                          
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End Resume Section -->
+
+  <!-- ======= Contact Section ======= -->
+  <section id="contact" class="contact">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Contact</h2>
+        <p>Hubungi Jika Perlu ???</p>
+      </div>
+
+      
+        <div class="col-md-6 mt-4 mt-md-0 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-share-alt"></i>
+            <h3>Social Profiles</h3>
+            <div class="social-links">
+              <a href="https://m.facebook.com/hasanulf1"><i class="icofont-facebook"></i></a>
+              <a href="https://www.instagram.com/hasanulfitriasyek/"><i class="icofont-instagram"></i></a>
+            </div>
+          </div>
+        </div>
+
+        
+        <div class="col-md-6 mt-4 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-phone-call"></i>
+            <h3>Call Me</h3>
+            <p>+628 5273 9402 23</p>
+          </div>
+        </div>
+      </div>
+
+      <form action="forms/contact.php" method="post" role="form" class="php-email-form mt-4">
+        <div class="form-row">
+          <div class="col-md-6 form-group">
+            <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+            <div class="validate"></div>
+          </div>
+          <div class="col-md-6 form-group">
+            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+            <div class="validate"></div>
+          </div>
+        </div>
+        <div class="form-group">
+          <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+          <div class="validate"></div>
+        </div>
+        <div class="form-group">
+          <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+          <div class="validate"></div>
+        </div>
+        <div class="mb-3">
+          <div class="loading">Loading</div>
+          <div class="error-message"></div>
+          <div class="sent-message">Terima Kasih Kamu Telah Memberi Saran Kepada Saya!</div>
+        </div>
+        <div class="text-center"><button type="submit">Send</button></div>
+      </form>
+
+    </div>
+  </section><!-- End Contact Section -->
+
+  <div class="credits">
+    <!-- All the links in the footer should remain intact. -->
+    <!-- You can delete the links only if you purchased the pro version. -->
+    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/personal-free-resume-bootstrap-template/ -->
+    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+  </div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
